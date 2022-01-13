06149fc793dcda62369a866d7090756ff364c4cf
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-01-13 09:59:46 +0100

    Add custom cursor and removing default cursor

---
+++ script/script.js
@@ -0,0 +1,24 @@
+const cursor = document.querySelector(".cursor");
+
+const growCursor = () => {
+  cursor.classList.add("is-down");
+};
+
+const shrinkCursor = () => {
+  cursor.classList.remove("is-down");
+};
+
+const moveCursor = (x, y) => {
+  cursor.style.left = x + "px";
+  cursor.style.top = y + "px";
+};
+
+document.addEventListener("mousedown", () => {
+  growCursor();
+});
+document.addEventListener("mouseup", () => {
+  shrinkCursor();
+});
+document.addEventListener("mousemove", (event) => {
+  moveCursor(event.pageX, event.pageY);
+});
