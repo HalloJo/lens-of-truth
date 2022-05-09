61708d00534e28ac372a48e51b7c6d5590765894
JorikVanRuiswijk <jorik.van.ruiswijk@mediamonks.com>
2022-05-09 15:56:39 +0200

    Set up canvas

--- script/script.js
+++ script/script.js
@@ -1,24 +1,48 @@
 const cursor = document.querySelector(".cursor");
-
-const growCursor = () => {
-  cursor.classList.add("is-down");
-};
-
-const shrinkCursor = () => {
-  cursor.classList.remove("is-down");
-};
+const canvasTag = document.querySelector("canvas.in");
 
 const moveCursor = (x, y) => {
   cursor.style.left = x + "px";
   cursor.style.top = y + "px";
 };
 
+const setupCanvas = (canvas) => {
+  const canvasWidth = window.innerWidth;
+  const canvasHeight = window.innerHeight;
+  const dpi = window.devicePixelRatio;
+
+  canvas.width = canvasWidth * dpi;
+  canvas.height = canvasHeight * dpi;
+  canvas.style.width = canvasWidth + "px";
+  canvas.style.height = canvasHeight + "px";
+
+  const context = canvas.getContext("2d");
+
+  context.scale(dpi, dpi);
+  context.fillStyle = "red";
+  context.rect(100, 100, 1000, 700);
+  context.fill();
+};
+
+setupCanvas(canvasTag);
+
+const growCursor = () => {
+  cursor.classList.add("is-down");
+  cursor.innerHTML = `<span>Let go please!</span>`;
+};
+
+const shrinkCursor = () => {
+  cursor.classList.remove("is-down");
+  cursor.innerHTML = `<span>Click me!</span>`;
+};
+
+document.addEventListener("mousemove", (event) => {
+  moveCursor(event.pageX, event.pageY);
+});
+
 document.addEventListener("mousedown", () => {
   growCursor();
 });
 document.addEventListener("mouseup", () => {
   shrinkCursor();
-});
-document.addEventListener("mousemove", (event) => {
-  moveCursor(event.pageX, event.pageY);
 });
