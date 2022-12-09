425a99a8dacc6dc644619a08b5bad80dedc0cf2b
HalloJo <jorikvanruiswijk@gmail.com>
2022-12-09 09:29:14 +0100

    Remove span from index, add stroke styling for canvas, add startdraw functionality

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const cursor = document.querySelector(".cursor");
 const canvasTag = document.querySelector("canvas.in");
 
+let isMouseDown = false;
+
 const moveCursor = (x, y) => {
   cursor.style.left = x + "px";
   cursor.style.top = y + "px";
@@ -19,30 +21,54 @@ const setupCanvas = (canvas) => {
   const context = canvas.getContext("2d");
 
   context.scale(dpi, dpi);
-  context.fillStyle = "red";
-  context.rect(100, 100, 1000, 700);
-  context.fill();
+  context.strokeStyle = "orange";
+  context.lineCap = "round";
+  context.lineWidth = "80";
+  context.lineJoin = "round";
+};
+
+const startDraw = (canvas, x, y) => {
+  const context = canvas.getContext("2d");
+
+  const colors = ["red", "green", "blue", "brown"];
+  const randomColor = Math.floor(Math.random() * colors.length);
+
+  context.strokeStyle = colors[randomColor];
+
+  context.moveTo(x, y);
+  context.beginPath();
+};
+
+const moveDraw = (canvas, x, y) => {
+  const context = canvas.getContext("2d");
+
+  if (isMouseDown) {
+    context.lineTo(x, y);
+    context.stroke();
+  }
 };
 
 setupCanvas(canvasTag);
 
 const growCursor = () => {
   cursor.classList.add("is-down");
-  cursor.innerHTML = `<span>Let go please!</span>`;
 };
 
 const shrinkCursor = () => {
   cursor.classList.remove("is-down");
-  cursor.innerHTML = `<span>Click me!</span>`;
 };
 
 document.addEventListener("mousemove", (event) => {
   moveCursor(event.pageX, event.pageY);
+  moveDraw(canvasTag, event.pageX, event.pageY);
 });
 
-document.addEventListener("mousedown", () => {
+document.addEventListener("mousedown", (event) => {
   growCursor();
+  startDraw(canvasTag, event.pageX, event.pageY);
+  isMouseDown = true;
 });
 document.addEventListener("mouseup", () => {
   shrinkCursor();
+  isMouseDown = false;
 });
